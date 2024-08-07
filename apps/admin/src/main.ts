dfaf3323b63604d70656b3ab4712c2684227ce38
이정호 <dlwjdgh1115@gmail.com>
2024-08-07 21:12:13 +0900

    add: swagger

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestudy Admin API')
+    .setDescription('Nestudy Admin API 명세서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestudy User API')
+    .setDescription('Nestudy User API 명세서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
